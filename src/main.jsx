417a7ecc80705ffcd01456ac6e1998c9ffff3810
Priyanshi Patel <130472715+PriyanshiPatel1205@users.noreply.github.com>
2024-09-17 19:20:58 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React, { Children } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Quiz from './components/Quiz.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path:'/',
+        element: <Home/>
+      },
+      {
+        path: '/quiz',
+        element: <Quiz/>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+)
